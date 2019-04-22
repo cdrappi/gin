b33b511441227d04cead11414c2d215d41ad7312
Christian Drappi <christiandrappi@gmail.com>
2019-04-22 13:34:14 -0500

    paste in token auth

--- gin/urls.py
+++ gin/urls.py
@@ -34,8 +34,7 @@ class EchoView(views.APIView):
     def post(self, request, *args, **kwargs):
         serializer = MessageSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 urlpatterns = [
