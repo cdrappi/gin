a2407086ab1881884d2625f30f5abda8d2c4f37a
Christian Drappi <christiandrappi@gmail.com>
2019-04-25 00:38:01 -0500

    comment line to use local npm against heroku server

--- src/components/api-config.js
+++ src/components/api-config.js
@@ -8,4 +8,7 @@ if (hostname === "heyhowsgame.herokuapp.com") {
   API_HOST = "http://localhost:8080";
 }
 
+// for using local npm to ping heroku server
+// API_HOST = "https://heyhowsgame.herokuapp.com";
+
 export default API_HOST;
