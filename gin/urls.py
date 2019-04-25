2c79a07d7045c14ec05234996a0ab6ed13996665
Christian Drappi <christiandrappi@gmail.com>
2019-04-24 23:25:11 -0500

    Deploy app 3 (#13)
    * update npm
    
    * update homepage in package.json
    
    * only point root / to app,

--- gin/urls.py
+++ gin/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('/', TemplateView.as_view(template_name='index.html')),
     path('token-auth/', obtain_jwt_token),
     path('admin/', admin.site.urls),
     path('dealer/', include('dealer.urls'))
