eb74ba2a194fb368b5ddb92a10816cba2c8c4510
Christian Drappi <christiandrappi@gmail.com>
2019-04-28 20:42:40 -0500

    try making all cards a button

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,14 @@ import API_HOST from "./api-config";
 class Card extends Component {
   constructor(props) {
     super(props);
+    let isDummyCard = props.rank === "?" && props.suit === "y";
+    let canDraw = props.action === "draw" && !props.inHand && !isDummyCard;
+    let canDiscard = props.action === "discard" && props.inHand;
     this.state = {
       isDiscard: !props.inHand && props.rank !== "?",
-      isDeck: !props.inHand && props.rank === "?"
+      canDiscard: canDiscard,
+      canDraw: canDraw,
+      isClickable: canDraw || canDiscard
     };
 
     // This binding is necessary to make `this` work in the callback
@@ -15,7 +20,7 @@ class Card extends Component {
   }
 
   handleClick() {
-    if (this.props.action === "discard" && this.props.inHand) {
+    if (this.state.canDiscard) {
       let card = this.props.rank + this.props.suit;
       fetch(`${API_HOST}/dealer/discard/`, {
         method: "POST",
@@ -32,7 +37,7 @@ class Card extends Component {
         .then(json => {
           this.props.refreshGames();
         });
-    } else if (this.props.action === "draw" && !this.props.inHand) {
+    } else if (this.state.canDraw) {
       fetch(`${API_HOST}/dealer/draw/`, {
         method: "POST",
         headers: {
@@ -52,18 +57,17 @@ class Card extends Component {
   }
 
   render() {
-    let cssIsLastDrawn = this.props.is_last_drawn ? "card-last-drawn" : "";
+    let lastDrawn = this.props.is_last_drawn ? "card-last-drawn" : "";
+    let clickable = this.state.isClickable ? "card-can-click" : "";
     return (
       <span>
-        <li>
-          <button
-            key={`${this.props.suit}${this.props.rank}`}
-            className={`card card-${this.props.suit} ${cssIsLastDrawn}`}
-            onClick={this.handleClick}
-          >
-            {this.props.rank}{" "}
-          </button>
-        </li>{" "}
+        <button
+          key={`${this.props.suit}${this.props.rank}`}
+          className={`card card-${this.props.suit} ${lastDrawn} ${clickable}`}
+          onClick={this.handleClick}
+        >
+          {this.props.rank}{" "}
+        </button>{" "}
       </span>
     );
   }
