72cbc90ab4a6562191cc85dc8d6c7f5ac1261d9d
Christian Drappi <christiandrappi@gmail.com>
2019-05-03 20:36:53 -0500

    ping correct api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Login from "./components/Login";
 import Games from "./components/Games";
-import UserList from "./components/UserList";
+import CreateGameSeries from "./components/CreateGameSeries";
 import "./App.css";
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <Login />
         <Games />
-        <UserList />
+        <CreateGameSeries />
       </div>
     );
   }
