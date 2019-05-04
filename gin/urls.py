08a774c3a09a5f2d140bbf3a89a7aa08f2016480
Christian Drappi <christiandrappi@gmail.com>
2019-05-04 00:36:03 -0500

    urls

--- gin/urls.py
+++ gin/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('token_auth/', obtain_jwt_token),
     path('admin/', admin.site.urls),
     path('dealer/', include('dealer.urls')),
+    path('play/', single_page_app),
+    path('/', single_page_app),
     path('', single_page_app),
 ]
