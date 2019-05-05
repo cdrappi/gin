47aa006ffa122bd61ae68b272bb0371436cada93
Christian Drappi <christiandrappi@gmail.com>
2019-05-05 17:42:42 -0500

    import cardfield

--- dealer/models/__init__.py
+++ dealer/models/__init__.py
@@ -3,5 +3,6 @@ from dealer.models.action_logging import (
     CardDrawn,
     StartingHand,
 )
+from dealer.models.card_field import CardField
 from dealer.models.game import Game
 from dealer.models.game_series import GameSeries
