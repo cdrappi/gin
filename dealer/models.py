faa13eb1496e0f611c0067429a555bbf3994d4a3
Christian Drappi <christiandrappi@gmail.com>
2019-05-05 14:34:15 -0500

    Use gin_utils repo for some calculations that dont touch state (#35)
    * trouble installing..
    
    * newer ginutils
    
    * upgrade version
    
    * make suits a list
    
    * use gin utils successfully
    
    * ricky.deal does not rake args
    
    * move even more to gin utils
    
    * upgrade ginutils
    
    * ricky sorted hand
    
    * upgrade ginutils
    
    * new gin_utils
    
    * back to try except'

--- dealer/models.py
+++ dealer/models.py
@@ -1,6 +1,5 @@
 import copy
 import datetime
-import itertools
 import random
 
 from django.contrib.auth.models import User
@@ -8,27 +7,14 @@ from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db import models
 from django.db.models import Q
 
-card_ranks = list('23456789TJQKA')
-card_suits = set('cdhs')
-
-card_choices = [(f'{rank}{suit}', f'{rank} of {suit}')
-                for rank in card_ranks for suit in card_suits]
-
-card_values = {
-    'A': 1,
-    'T': 10,
-    'J': 11,
-    'Q': 12,
-    'K': 13,
-    **{d: int(d) for d in '23456789'}
-}
+from gin_utils import deck, ricky
 
 
 class CardField(models.CharField):
     """ e.g. 'Ah', 'Kc', '4s', etc. """
 
     def __init__(self, *args, **kwargs):
-        kwargs = {**kwargs, 'max_length': 2, 'choices': card_choices}
+        kwargs = {**kwargs, 'max_length': 2, 'choices': deck.card_choices}
         super().__init__(*args, **kwargs)
 
 
@@ -361,118 +347,7 @@ class Game(models.Model):
         :return: (int)
         """
         users_hand = self.users_hand(user)
-        return self.hand_points(users_hand)
-
-    @staticmethod
-    def yield_hand_combos(hand):
-        for combo_3 in itertools.combinations(hand, 3):
-            remaining_cards = [c for c in hand if c not in combo_3]
-            if len(remaining_cards) == 5:
-                # not using itertools.combinations(remaining_cards, 1) here,
-                # because it returns a tuple with 1 item rather than sole item
-                for left_out_card in remaining_cards:
-                    combo_4 = [c for c in remaining_cards if c != left_out_card]
-                    yield combo_3, combo_4, left_out_card
-            elif len(remaining_cards) == 4:
-                yield combo_3, remaining_cards
-            else:
-                raise Exception(f"incorrect number of cards ({len(hand)}) in hand")
-
-    @classmethod
-    def combos_points(cls, combo_3, combo_4):
-        return cls.combo_points(combo_3) + cls.combo_points(combo_4)
-
-    @classmethod
-    def hand_points(cls, hand):
-        """
-
-        :param hand: ([str]) list of cards
-        :return: (int)
-        """
-        points = 14 * 7
-        for combo_3, combo_4, *_ in cls.yield_hand_combos(hand):
-            combo_points = cls.combos_points(combo_3, combo_4)
-            if combo_points == 0:
-                return 0
-            points = min(combo_points, points)
-
-        return points
-
-    @staticmethod
-    def sort_cards(cards):
-        return sorted(cards, key=lambda c: card_values[c[0]])
-
-    def sorted_hand(self, hand):
-        """
-        :param hand: ([str])
-        :return: ([str])
-        """
-        combo_3, combo_4, *maybe_last_card = min(self.yield_hand_combos(hand),
-                                                 key=lambda k: self.combos_points(k[0], k[1]))
-        sorted_combo_3 = self.sort_cards(combo_3)
-        sorted_combo_4 = self.sort_cards(combo_4)
-        sorted_hand = sorted_combo_3 + sorted_combo_4 + maybe_last_card
-        rank_points = self.calculate_points([c[0] for c in sorted_hand])
-        combo_points = self.combos_points(combo_3, combo_4)
-        if maybe_last_card:
-            combo_points += self.calculate_points(maybe_last_card[0][0])
-
-        if rank_points == combo_points:
-            return self.sort_cards(sorted_hand)
-        elif self.combo_points(combo_4) < self.combo_points(combo_3):
-            return sorted_combo_4 + sorted_combo_3 + maybe_last_card
-        else:
-            return sorted_combo_3 + sorted_combo_4 + maybe_last_card
-
-    @classmethod
-    def combo_points(cls, card_combo):
-        """
-
-        :param card_combo: ([str])
-        :return: (int)
-        """
-        first_rank, first_suit = card_combo[0]
-        card_ranks = [card[0] for card in card_combo]
-
-        if all(r == first_rank for r in card_ranks):
-            # all cards are same rank
-            return 0
-
-        if any(card[1] != first_suit for card in card_combo):
-            # cards are not of same rank and not of same suit,
-            # so return the points
-            return cls.calculate_points(card_ranks)
-
-        # NOTE: if we get to this point, cards have same suit
-        # so we just need to check if they make a valid 3- or 4-straight
-
-        # handle fact that aces can be 1 or 14
-        values_ace_as_1 = sorted(card_values[r] for r in card_ranks)
-        rank_combos = [values_ace_as_1]
-        if any(r == 'A' for r in card_ranks):
-            rank_combos.append(sorted(14 if value == 1 else value for value in values_ace_as_1))
-
-        for rank_combo in rank_combos:
-            if cls.ranks_make_a_straight(rank_combo):
-                return 0
-
-        return cls.calculate_points(card_ranks)
-
-    @staticmethod
-    def calculate_points(card_ranks):
-        return sum(card_values[r] for r in card_ranks)
-
-    @staticmethod
-    def ranks_make_a_straight(rank_combo):
-        """
-
-        :param rank_combo: ([int])
-        :return: (bool)
-        """
-        for r1, r2 in zip(rank_combo[:-1], rank_combo[1:]):
-            if r1 != r2 - 1:
-                return False
-        return True
+        return ricky.hand_points(users_hand)
 
     def get_action(self, user):
         """
@@ -530,7 +405,7 @@ class Game(models.Model):
         if final_info['action'] == "discard":
             final_info['drawn_card'] = self.last_draw
 
-        hand = self.sorted_hand(self.users_hand(user))
+        hand = ricky.sort_hand(self.users_hand(user))
 
         def transform_loc(loc):
             """
@@ -549,7 +424,7 @@ class Game(models.Model):
         }
         return {
             'hand': hand,
-            'points': self.hand_points(hand),
+            'points': ricky.hand_points(hand),
             'top_of_discard': self.top_of_discard,
             'deck_length': len(self.deck),
             'last_completed_turn': (
@@ -606,29 +481,13 @@ class Game(models.Model):
         return self.users_hand(opponent)
 
     @staticmethod
-    def random_deck():
-        deck = [card for card, _ in card_choices]
-        random.shuffle(deck)
-        return deck
-
-    @staticmethod
-    def deal_new_game():
-        deck = Game.random_deck()
-        return {
-            'p1_hand': deck[0:7],
-            'p2_hand': deck[7:14],
-            'discard': [deck[14]],
-            'deck': deck[15:],
-        }
-
-    @staticmethod
     def new_game(game_series):
         """
 
         :param game_series: (GameSeries)
         :return:
         """
-        dealt_game = Game.deal_new_game()
+        dealt_game = ricky.deal_new_game()
         p1_goes_first = random.choice([True, False])
         game = Game(
             series=game_series,
