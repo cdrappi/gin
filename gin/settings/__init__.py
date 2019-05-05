5417fb1bc9a08e8abfb0ca9402699277f1d068f6
Christian Drappi <christiandrappi@gmail.com>
2019-05-04 23:46:14 -0500

    undo that it broke the app

--- gin/settings/__init__.py
+++ gin/settings/__init__.py
@@ -118,7 +118,7 @@ SECURE_SSL_REDIRECT = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
