3f4140bc09dffafdcc8d000ab4b3f1d3a3a6a3af
Christian Drappi <christiandrappi@gmail.com>
2019-05-04 23:43:05 -0500

    try changing STATIC_ROOT to get favicon visible

--- gin/settings/__init__.py
+++ gin/settings/__init__.py
@@ -118,7 +118,7 @@ SECURE_SSL_REDIRECT = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
